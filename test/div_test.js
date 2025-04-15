e3c29f95ea32968ff7c373123dfc0217f29ee1bf
elugui <elisson.lugui@gmail.com>
2025-04-15 11:19:36 -0300

    add test coverage

---
+++ test/div_test.js
@@ -0,0 +1,5 @@
+const  div = require('../src/div');
+
+test('4 / 2 to equal 2', () => {
+    expect(div(4, 2)).toBe(2);
+});
---
+++ test/mult_test.js
@@ -0,0 +1,5 @@
+const  mult = require('../src/mult');
+
+test('3 * 2 to equal 6', () => {
+    expect(mult(3, 2)).toBe(6);
+});
